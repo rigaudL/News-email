1cf62c988a21dd288de6fa6fd3deb5af700e4daf
rigaudL <121980656+rigaudL@users.noreply.github.com>
2024-04-12 03:11:08 -0400

    Update main.py
    final

--- News email/main.py
+++ News email/main.py
@@ -1,14 +1,12 @@
 import requests
 from backend_contact import send_email
 
-# Ensure your API key is included in the URL
 api_key = "c67d19eb481749fc9b540e35915579c3"
 url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={api_key}"
 
 response = requests.get(url)
 content = response.json()
 
-# Check if the response contains the expected 'articles' key
 if "articles" in content:
     # Initialize an empty string to store the body of the email
     body = ""
@@ -19,10 +17,8 @@ if "articles" in content:
         # Concatenate the title, description, URL, and additional lines
         body += f"{article['title']}\n{description}\n{article['url']}\n\n"
 
-    # Define the subject for the email
     subject = "Top Business Headlines from NewsAPI"
 
-    # Call the send_email function with the subject and message
     send_email(subject, body)  # This line sends the email
 else:
     print("Error: Unable to retrieve articles from NewsAPI.")
